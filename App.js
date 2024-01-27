bbe24420e170da0609863348ba48fcccb813fd13
LYW14 <146258004+LYW14@users.noreply.github.com>
2024-01-27 18:38:19 -0500

    Uploaded files?

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SiteNav from './components/common/SiteNav';
+import SiteFooter from './components/common/SiteFooter';
+
+function App() {
+  return (
+    <div>
+      <SiteNav/>
+      <SiteFooter />
+    </div>
+  );
+}
+
+export default App;
---
+++ HomePage.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Container from 'react-bootstrap/Container';
+
+function HomePage(){
+    return 
+    (
+        
+    )
+
+}
+
+export default HomePage;
